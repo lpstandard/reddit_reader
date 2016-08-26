e0e5d734e50c62368f3d2bf5b92b66ecae129b39
Tony Kung <lps.tony@gmail.com>
2016-08-25 20:17:45 -0500

    initial commit

---
+++ www/js/app.js
@@ -0,0 +1,38 @@
+(function(){
+
+  var app = angular.module('myreddit', ['ionic'])
+
+  app.controller('RedditCtrl', function($http, $scope){
+
+    $scope.stories = [];
+     
+
+    $scope.loadOlderStories = function(){
+      var params = {};
+      if($scope.stories.length > 0){
+        params['after'] = $scope.stories[$scope.stories.length - 1].name;
+      }
+      $http.get('http://www.reddit.com/.json', {params: params})
+      .success(function(response){
+        angular.forEach(response.data.children, function(child){
+          $scope.stories.push(child.data);
+        });
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+      });
+    }
+
+  });
+
+  app.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      if(window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if(window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+}());
+
